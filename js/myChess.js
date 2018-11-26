1c6ba94b2d4a71f918f581d8e673508e752fc241
Calvin Studebaker <calvin@codehs.com>
2018-11-25 23:41:22 -0700

    computer picks the move that results in best material score

--- js/myChess.js
+++ js/myChess.js
@@ -1,12 +1,27 @@
 var game;
 var board;
 
+var PIECE_POINTS = {
+    'p' : -1,
+    'k' : -3,
+    'b' : -3,
+    'r' : -5,
+    'q' : -9,
+    'k' : -1000,
+    'P' : 1,
+    'K' : 3,
+    'B' : 3,
+    'R' : 5,
+    'Q' : 9,
+    'K' : 1000
+}
+
 function start(){
     setupListeners();
     newGame();
 }
 
-function setupListeners(){
+var setupListeners = function(){
     $('.reset-game').hide();
     $('#new-game').click(function(){
         $('.reset-game').hide();
@@ -14,12 +29,12 @@ function setupListeners(){
     });
 }
 
-function newGame(){
+var newGame = function(){
     game = new Chess();
     board = setupBoard();
 }
 
-function gameIsOver(){
+var gameIsOver = function(){
     return game.game_over() || game.in_draw() || game.moves().length == 0
 }
 
@@ -60,7 +75,7 @@ var onSnapEnd = function() {
     board.position(game.fen());
 };
 
-function setupBoard(){
+var setupBoard = function(){
     var boardConfig = {
         draggable: true,
         dropOffBoard: 'snapback',
@@ -83,7 +98,41 @@ var makeRandomMove = function() {
     board.position(game.fen());
 };
 
+// Return the material score of the board
+// + points for white pieces
+// - points for black pieces
+var scoreBoard = function(asciiBoard){
+    var rows = asciiBoard.split('\n');
+    var score = 0;
+    for(var i = 1; i < 9; i++) {
+        for(var index = 0; index < rows[i].length; index++){
+            var char = rows[i][index];
+            var value = PIECE_POINTS[char];
+            if (value != undefined){
+                score += value;
+            }
+        }
+    }
+    return score;
+}
+
+// Generate all possible moves and pick the move with the optimal score
 var makeComputerMove = function(){
-    makeRandomMove();
+    var possibleMoves = game.moves();
+    var bestMove = null;
+    var bestScore = 99999;
+    for(var i = 0; i < possibleMoves.length; i++){
+        game.move(possibleMoves[i]);
+        var score = scoreBoard(game.ascii());
+        game.undo();
+
+        if (score < bestScore) {
+            bestScore = score;
+            bestMove = possibleMoves[i];
+        }
+    }
+
+    game.move(bestMove);
+    board.position(game.fen());
 }
 

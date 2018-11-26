a2285867f7a0e5a284886233cb2983602a926689
Calvin Studebaker <calvin@codehs.com>
2018-11-25 21:51:14 -0700

    chess move validation

--- js/myChess.js
+++ js/myChess.js
@@ -1,3 +1,37 @@
+var chess;
+
 function start(){
-    var board = ChessBoard('board', 'start');
+    chess = new Chess();
+    setupBoard();
 }
+
+function isValidMove(target, validMoves){
+    console.log(validMoves);
+    for(var i = 0; i < validMoves.length; i++) {
+        if(validMoves[i].indexOf(target) != -1){
+            return true;
+        }
+    }
+    return false;
+}
+
+function setupBoard(){
+    var onDrop = function(source, target, piece, newPos, oldPos, orientation){
+        var validMoves = chess.moves({square: source})
+        if (!isValidMove(target, validMoves)) {
+            return 'snapback'
+        }
+        else {
+            chess.move({from: source, to: target});
+        }
+    };
+
+    var boardConfig = {
+        draggable: true,
+        dropOffBoard: 'snapback',
+        position: 'start',
+        onDrop: onDrop,
+    };
+    var board = ChessBoard('board', boardConfig);
+}
+
